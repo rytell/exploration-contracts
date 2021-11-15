76096b246f20dbe08d96e9950db301d4fefdd275
Pedro Medina <pedromedina190@gmail.com>
2021-11-15 17:25:29 -0300

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+const mnemonic = process.env.MNEMONIC;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    fuji: {
+      accounts: {
+        mnemonic
+      },
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gas: 8000000,
+      gasPrice: 225000000000,
+      timeout: 30000,
+      chainId: 43113
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD"
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
+
+export default config;
