d262766a3f729b86c4537b0f8f4e1e356dff8860
Pedro Medina <pedromedina190@gmail.com>
2021-11-15 18:15:10 -0300

    add example contracts for novax

---
+++ contracts/Novax/Novax.sol
@@ -0,0 +1,5 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Novax {
+  function getParam3(string memory key) public view returns (address) {}
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,9 +41,6 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD"
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
 
